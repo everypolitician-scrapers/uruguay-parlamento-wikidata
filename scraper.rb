0e7d73ad495ed986eaea6b3c0c694784100a32fb
Tony Bowden <tony@tmtm.com>
2016-03-28 19:12:57 +0100

    fixup!

--- scraper.rb
+++ scraper.rb
@@ -5,4 +5,4 @@ require 'wikidata/fetcher'
 
 en_names = WikiData::Category.new( 'Category:Members of the Chamber of Deputies of Uruguay', 'en').member_titles
 es_names = WikiData::Category.new( 'Categoría:Diputados de Uruguay', 'es').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { es: es_name, en: en_names }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(names: { es: es_names, en: en_names }, output: false)
